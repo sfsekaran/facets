d7b6398f7488f2f261e3f6589ea83b7d3fc059b0
Sathya Sekaran <sfsekaran@gmail.com>
2018-07-29 18:58:09 -0700

    add comments into post show page

--- blog/rails_blog/db/schema.rb
+++ blog/rails_blog/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_30_012409) do
+ActiveRecord::Schema.define(version: 2018_07_30_013907) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.string "handle"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
